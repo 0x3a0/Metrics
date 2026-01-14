fe4babb277d036d859d5452c2c9beea702e0e489
Ice1and <1185330343@qq.com>
2026-01-14 19:51:39 +0800

    修改sidebar布局

--- frontend/src/components/sidebars/sidebarLayout.vue
+++ frontend/src/components/sidebars/sidebarLayout.vue
@@ -24,27 +24,28 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="relative flex w-full h-svh bg-zinc-900/3">
+  <div class="relative flex w-full min-h-svh bg-zinc-900/3">
     <!-- Sidebar container -->
-    <aside class="fixed inset-y-0 left-0 w-64 flex flex-col">
-      <!-- Sidebar header -->
-      <div class="sticky top-0 shrink-0 px-4 py-3.5">   <!-- shrink-0 防止flex item缩小 -->
-        <button class="flex items-center text-base/6 text-left font-semibold">
-          <img src="@/assets/logo.svg" alt="">
-          <span>{{ props.webName }}</span>
-        </button>
-      </div>
+    <aside class="fixed inset-y-0 left-0 w-64">
+      <div class="flex flex-col h-full">
+        <!-- Sidebar header -->
+        <div class="shrink-0 px-4 py-3.5">   <!-- shrink-0 防止flex item缩小 -->
+          <button class="flex items-center text-base/6 text-left font-semibold">
+            <img src="@/assets/logo.svg" alt="">
+            <span>{{ props.webName }}</span>
+          </button>
+        </div>
 
-      <!-- Sidebar content -->
-      <div class="flex-1 flex flex-col overflow-y-auto p-4 gap-0.5">
-        <slot name="sidebar-content"></slot>
-      </div>
+        <!-- Sidebar content -->
+        <div class="flex-1 flex flex-col overflow-y-auto p-4 gap-0.5">
+          <slot name="sidebar-content"></slot>
+        </div>
 
-      <!-- Sidebar footer-->
-      <div class="sticky bottom-0 shrink-0 px-4 py-3.5">
-        <slot name="sidebar-footer"></slot>
+        <!-- Sidebar footer-->
+        <div class="shrink-0 px-4 py-3.5">
+          <slot name="sidebar-footer"></slot>
+        </div>
       </div>
-
     </aside>
 
     <!-- skeleton-->
@@ -52,7 +53,7 @@ onMounted(() => {
 
     <!-- main -->
     <div class="flex-1 flex flex-col ml-64 pb-2 pt-2 pr-2">
-      <div class="grow border border-zinc-950/10 bg-white rounded-lg shadow-xs px-9 py-8">
+      <div class="grow border border-zinc-950/10 bg-white rounded-lg shadow-xs px-10 py-9">
         <h1 class="text-2xl/6 font-semibold">{{ timeQuantum }}</h1>
 
         <!-- main container -->
